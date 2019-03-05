aa54d9acc2df9fc33feaac2c5d8684cbde8bd30f
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2019-03-04 23:03:55 -0500

    Initial

---
+++ Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour {
+
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(90, objectToFollow.rotation.eulerAngles.y + 90, 90);
+        transform.position = objectToFollow.position + objectToFollow.up * cameraHeight;
+    }
+
+    public Transform objectToFollow;
+    public float cameraHeight = 10;
+}
