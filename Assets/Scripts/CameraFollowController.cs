ae3bb28d1410b64a36671892e8f7bbd3c02930b7
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2019-03-07 22:28:11 -0500

    fixed camera and track

--- Assets/Scripts/CameraFollowController.cs
+++ Assets/Scripts/CameraFollowController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowController : MonoBehaviour {
-
-
+public class CameraFollowController : MonoBehaviour
+{
     private void FixedUpdate()
     {
+        Vector3 dir = objectToFollow.transform.forward;
+
         transform.rotation = Quaternion.Euler(90, objectToFollow.rotation.eulerAngles.y + 90, 90);
-        transform.position = objectToFollow.position + objectToFollow.up * cameraHeight;
+        transform.position = new Vector3(objectToFollow.position.x, cameraHeight, objectToFollow.position.z);
+        transform.position += dir * distance;
     }
 
     public Transform objectToFollow;
     public float cameraHeight = 10;
+    public float distance = 3;
 }
