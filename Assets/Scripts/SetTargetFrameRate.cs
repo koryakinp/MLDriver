4d8272a58209a3e3a85ea892bbb607db85b2b579
koryakinp <koryakinp@koryakinp.com>
2019-12-05 22:57:10 -0500

    minor fixes

---
+++ Assets/Scripts/SetTargetFrameRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+ 
+ public class SetTargetFrameRate : MonoBehaviour 
+ {
+     public int targetFrameRate = 10;
+ 
+     private void Start()
+     {
+         QualitySettings.vSyncCount = 0;
+         Application.targetFrameRate = targetFrameRate;
+         Debug.Log("Start");
+     }
+ }
